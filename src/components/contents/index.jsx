479da629a28ff96ebc82844b9151dc5b337965c3
Diko <cikociko612@gmail.com>
2020-03-30 08:56:14 +0700

    crud

--- src/components/contents/index.jsx
+++ src/components/contents/index.jsx
@@ -1,27 +1,41 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import DataSantri from './dataSantri'
 import PageNotFound from './page404'
 
 const Contents = (props) => {
-   return ( 
-      <div className="container-fluid"> 
-      { props.dataSantri ? 
-            <DataSantri 
-            value             = {props.value}
-            searchedSantri    = {props.searchedSantri}
-            newDataSantri     = {props.newDataSantri}
-            onHandleUpdate    = {props.onHandleUpdate}
-            postDataSantri    = {props.postDataSantri}
-            onHandleInput     = {props.onHandleInput}
-            dataSantri        = {props.dataSantri}
-            onHandleDelete    = {props.onHandleDelete}
-            dataUpdate        = {props.dataUpdate}
-            simpanDataSantri  = {props.simpanDataSantri}
-            />
-            : <PageNotFound/>
-      }
-      </div>
-   )
+  return (
+    <div className='container-fluid'>
+      {props.dataSantri
+        ? <DataSantri
+          value={props.value}
+          searchedSantri={props.searchedSantri}
+          newDataSantri={props.newDataSantri}
+          onHandleUpdate={props.onHandleUpdate}
+          postDataSantri={props.postDataSantri}
+          onHandleInput={props.onHandleInput}
+          dataSantri={props.dataSantri}
+          onHandleDelete={props.onHandleDelete}
+          dataUpdate={props.dataUpdate}
+          simpanDataSantri={props.simpanDataSantri}
+        // eslint-disable-next-line react/jsx-closing-bracket-location
+        />
+        : <PageNotFound />}
+    </div>
+  )
+}
+
+Contents.propTypes = {
+  onHandleUpdate: PropTypes.func,
+  onHandleInput: PropTypes.func,
+  onHandleDelete: PropTypes.func,
+  simpanDataSantri: PropTypes.array,
+  searchedSantri: PropTypes.array,
+  newDataSantri: PropTypes.array,
+  value: PropTypes.string,
+  postDataSantri: PropTypes.array,
+  dataSantri: PropTypes.array,
+  dataUpdate: PropTypes.func
 }
 
 export default Contents
